2c89eb913b809c7b4644521689e475b83b5a08e8
robsonvleite <robsonvleite@gmail.com>
2022-04-02 08:20:13 -0300

    Update Save Example

--- example/save_example.php
+++ example/save_example.php
@@ -12,47 +12,73 @@ use Example\Models\User;
 /*
  * USER MODEL
  */
-print "new user model";
+$create = false;
+if ($create) {
+    echo "<h1>Create:</h1>";
 
-$user = new User;
-$user->first_name = "Robson";
-$user->last_name = "V. Leite";
-$user->genre = "m";
-$user->save();
+    $user = new User;
+    $user->first_name = "Robson";
+    $user->last_name = "V. Leite";
+    $user->genre = "m";
+    if ($user->save()) {
+        echo "Usuário cadastrado";
+        var_dump($user->data());
+    } else {
+        echo "<b>Erro ao cadastrar:</b> {$user->fail()->getMessage()}";
+        var_dump($user->fail());
+    }
 
-var_dump($user);
+    die;
+}
 
 /*
  * UPDATE USER
  */
-print "update user";
+$update = false;
+if ($update) {
+    echo "<h1>Update:</h1>";
 
-$name = ["Robson", "Kaue", "Gustavo", "João"];
+    $name = ["Marcos", "Marcelo", "Ricardo", "João"];
+    $user = (new User())->findById(4);
 
-$user = (new User())->findById($user->id);
+    if ($user) {
+        $user->first_name = $name[rand(0, 3)];
+        if ($user->save()) {
+            echo "Usuário atualizado";
+            var_dump($user->data());
+        } else {
+            echo "<b>Erro ao atualizar:</b> {$user->fail()->getMessage()}";
+            var_dump($user->fail());
+        }
+    } else {
+        echo "<p>Usuário não encontrado, informe o id do usuário!</p>";
+    }
 
-if ($user) {
-    $user->first_name = $name[rand(0, 3)];
-    $user->save();
-    var_dump($user);
-} else {
-    echo "<h2>Not User</h2>";
-}
-
-/*
- * ADDR MODEL PRIMARY KEY CHANGED
- */
-print "addr model";
-
-$addr = new Address;
-$addr->user_id = $user->id;
-$addr->address = "Rua do {$user->first_name}, Nº " . rand(1, 1000);
-$address = $addr->save();
-
-if (!$address) {
-    echo "<h2>{$addr->fail()->getMessage()}</h2>";
-    var_dump($addr->fail());
     die;
 }
 
-var_dump($addr);
+/*
+ * ADDR MODEL
+ */
+$addr = false;
+if ($addr) {
+    echo "<h1>Addr Model Example</h1>";
+
+    $user = (new User())->findById(5);
+
+    $addr = new Address;
+    $addr->user_id = $user->id;
+    $addr->address = "Rua do {$user->first_name}, Nº " . rand(1, 1000);
+
+    if ($addr->save()) {
+        echo "Endereço atualizado!";
+        var_dump($addr->data());
+        var_dump($addr->user()->data());
+    } else {
+        echo "<h2>{$addr->fail()->getMessage()}</h2>";
+        var_dump($addr->fail());
+        die;
+    }
+
+    die;
+}
